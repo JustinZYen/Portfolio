6b51f4c7f9704673ff018fe88b8f86f0765130dd
Justin Yen <justzyen@gmail.com>
2024-09-30 12:12:44 -0700

    fixed commit where I accidentally put in authentication info

--- routes/contact.js
+++ routes/contact.js
@@ -1,16 +1,14 @@
-var express = require('express');
-var mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require("mongoose");
 
-var router = express.Router();
+const router = express.Router();
 /* GET users listing. */
 // pass = NOkaankbbIllI0v3
-const mongoURI = process.env.AZURE_COSMOS_CONNECTIONSTRING || "mongodb+srv://justzyen:NOkaankbbIllI0v3@cluster0.mlg66.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI = process.env.AZURE_COSMOS_CONNECTIONSTRING || process.env.ATLAS_MONGODB_URI;
+console.log("URI being used for mongo is"+mongoURI);
 mongoose.connect(mongoURI);
 
 const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
 
 router.get('/', function(req, res, next) {
   res.render("contact");
@@ -18,6 +16,8 @@ router.get('/', function(req, res, next) {
 
 router.post('/submit', function(req, res, next) {
   console.log(req.body);
+  const kitty = new Cat({ name: req.body.name });
+  kitty.save().then(() => console.log('meow'));
   res.redirect("./success");
 });
 
