92b6f05f0b7b896b203cce4ba722c3ab07a6c1fb
Justin Yen <justzyen@gmail.com>
2024-09-30 11:16:28 -0700

    test add mongodb

--- routes/contact.js
+++ routes/contact.js
@@ -1,7 +1,17 @@
 var express = require('express');
-var router = express.Router();
+var mongoose = require("mongoose");
 
+var router = express.Router();
 /* GET users listing. */
+// pass = NOkaankbbIllI0v3
+const mongoURI = process.env.AZURE_COSMOS_CONNECTIONSTRING || "mongodb+srv://justzyen:NOkaankbbIllI0v3@cluster0.mlg66.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongoURI);
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+
 router.get('/', function(req, res, next) {
   res.render("contact");
 });
